6e901e1937259816c49de71cf681b4dfc83c7b49
Philipp Bosch <hello@pb.io>
2018-10-15 14:59:09 +0200

    Add markdown-to-pdf script.

---
+++ markdown-to-pdf/README.md
@@ -0,0 +1,34 @@
+# Markdown to PDF
+
+This script allows you to convert Deckset presentations to PDF files
+from the command line.
+
+
+## Installation
+
+1. Copy _markdown-to-pdf.sh_ from this folder to a directory in your `$PATH`.  
+   `mv markdown-to-pdf.sh /usr/local/bin/markdown-to-pdf`
+2. Make the script executable  
+   `chmod +x /path/to/markdown-to-pdf`
+
+
+## Usage
+
+### Convert a single file
+
+```shell
+$ markdown-to-pdf /path/to/file.md
+```
+
+The PDF file will be placed right next to the source file, e.g. _/path/to/file.pdf_.
+
+
+### Convert multiple files
+
+```shell
+$ markdown-to-pdf /path/to/file-one.md /path/to/file-two.md /path/to/file-three.md
+```
+
+### Author
+
+**TODO**
---
+++ markdown-to-pdf/markdown-to-pdf.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+show_usage() {
+    echo "Markdown to PDF"
+    echo ""
+    echo "Usage: $0 [options] file [file...]"
+    echo ""
+    echo "Options:"
+    echo "  -f   Overwrite existing output files."
+    echo "  -h   Print usage."
+}
+
+OVERWRITE=0
+
+while getopts fh option
+do
+    case "${option}"
+    in
+        f)
+            OVERWRITE=1;
+            shift;;
+        h)
+            show_usage;
+            exit 0;
+    esac
+done
+
+if [ $# -eq 0 ]
+then
+    show_usage
+    exit 1
+fi
+
+for FILE in "$@" ; do
+    md_file="$(cd "$(dirname "$FILE")"; pwd)/$(basename "$FILE")"
+    pdf_file="${FILE%.*}.pdf"
+
+    if [ ! -e $md_file ]
+    then
+        echo "Input file $md_file does not exist!"
+        exit 2
+    fi
+
+    if [ -e $pdf_file ] && [ $OVERWRITE = 0 ]
+    then
+        echo "Error: output file $pdf_file exists. Please use -f option if you want to overwrite it."
+        exit 3
+    fi
+
+    osascript <<EOF
+        on run argv
+            set md_file to "$md_file"
+            set md_file to POSIX file md_file
+            set out_file to "$pdf_file"
+            set out_file to POSIX file out_file
+            tell application "Deckset Private Beta"
+                activate
+                open file md_file
+                tell document 1
+                    activate
+                    export to out_file
+                end tell
+            end tell
+        end run
+EOF
+done
